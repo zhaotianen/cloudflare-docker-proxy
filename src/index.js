d36102f43e5f54a77cfd6c8c8b0da371eabff682
zhaotianen <909442787@qq.com>
2024-06-13 13:55:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ygys.xyz": "https://registry-1.docker.io",
+  "quay.ygys.xyz": "https://quay.io",
+  "gcr.ygys.xyz": "https://gcr.io",
+  "k8s-gcr.ygys.xyz": "https://k8s.gcr.io",
+  "k8s.ygys.xyz": "https://registry.k8s.io",
+  "ghcr.ygys.xyz": "https://ghcr.io",
+  "cloudsmith.ygys.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
